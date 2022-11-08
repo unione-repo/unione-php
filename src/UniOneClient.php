e75e777efa8cb82612e1d9fcd7a73f8369149092
Vlad Denysiuk <v.denysiuk@dev-branch.com>
2022-11-08 13:15:44 +0200

    style: Fix code styling
    Fixed code styling in the UniOneClient

--- src/UniOneClient.php
+++ src/UniOneClient.php
@@ -11,110 +11,112 @@ use Psr\Http\Message\UriInterface;
 /**
  * This class is the base class for the UniOne SDK.
  */
-final class UniOneClient {
+final class UniOneClient
+{
+    /**
+     * The HTTP client.
+     *
+     * @var Client
+     */
+    private Client $httpClient;
 
-  /**
-   * The HTTP client.
-   *
-   * @var Client
-   */
-  protected Client $httpClient;
+    /**
+     * UniOne Instance.
+     *
+     * @var string|UriInterface
+     * @todo: Make it configurable.
+     */
+    private static $endpoint = 'https://eu1.unione.io/en/transactional/api/v1/';
 
-  /**
-   * UniOne Instance.
-   *
-   * @var string|UriInterface
-   * @todo: Make it configurable.
-   */
-  static protected $endpoint = 'https://eu1.unione.io/en/transactional/api/v1/';
+    /**
+     * The API key.
+     *
+     * @var string
+     */
+    private string $apiKey;
 
-  /**
-   * The API key.
-   *
-   * @var string
-   */
-  protected string $apiKey;
-
-  public function __construct() {
-    $this->httpClient = new Client([
-      'base_uri' => $this::$endpoint
-    ]);
-  }
-
-  /**
-   * Set the API key.
-   *
-   * @param string $apiKey The API key.
-   */
-  public function setApiKey(string $apiKey): void {
-    $this->apiKey = $apiKey;
-  }
-
-  /**
-   * Send a request to the UniOne API.
-   *
-   * @param array $body The request body.
-   * @param array $head The request headers.
-   *
-   * @return string
-   * @throws \GuzzleHttp\Exception\GuzzleException
-   */
-  public function sender(array $body, array $head = []): string {
-    $responseHeader = [
-      'Content-Type' => 'application/json',
-      'Accept' => 'application/json',
-      'X-API-KEY' => $this->apiKey,
-      'X-Mailer' => 'UniOne SDK-PHP agent'
-    ];
-
-    // Build body for request.
-    // @todo: Refactor this and create a special class for it.
-    $responseBody = [
-      "message" => [
-        "recipients" => [
-          [
-            "email" => $body['recipients_email'] ?? NULL,
-            "substitutions" => $body['recipients_substitutions'] ?? NULL,
-            "metadata" => $body['recipients_metadata'] ?? NULL,
-          ]
-        ],
-        "template_id" => $body['template_id'] ?? NULL,
-        "skip_unsubscribe" => $body['skip_unsubscribe'] ?? NULL,
-        "global_language" => $body['global_language'] ?? NULL,
-        "template_engine" => $body['template_engine'] ?? NULL,
-        "global_substitutions" => $body['global_substitutions'] ?? NULL,
-        "global_metadata" => $body['global_metadata'] ?? NULL,
-        "body" => [
-          "html" => $body['body_html'] ?? NULL,
-          "plaintext" => $body['body_plaintext'] ?? NULL,
-          "amp" => $body['body_amp'] ?? NULL,
-        ],
-        "subject" => $body['subject'] ?? NULL,
-        "from_email" => $body['from_email'] ?? NULL,
-        "from_name" => $body['from_name'] ?? NULL,
-        "reply_to" => $body['reply_to'] ?? NULL,
-        "track_links" => 0,
-        "track_read" => 0,
-        "headers" => $body['headers'] ?? NULL,
-        "attachments" => $body['attachments'] ?? NULL,
-        "inline_attachments" => $body['inline_attachments'] ?? NULL,
-        "options" => $body['options'] ?? NULL,
-      ]
-    ];
-
-    try {
-      // Send request.
-      $response = $this->httpClient->post('email/send.json', [
-          'headers' => $head + $responseHeader,
-          'json' => $responseBody
-        ]
-      );
-
-      return $response->getBody()->getContents();
-    } catch (BadResponseException $e) {
-      // handle exception or api errors.
-      return $e->getResponse()->getBody()->getContents();
+    public function __construct()
+    {
+        $this->httpClient = new Client([
+          'base_uri' => $this::$endpoint,
+        ]);
     }
-  }
 
+    /**
+     * Set the API key.
+     *
+     * @param string $apiKey the API key
+     */
+    public function setApiKey(string $apiKey): void
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * Send a request to the UniOne API.
+     *
+     * @param array $body the request body
+     * @param array $head the request headers
+     *
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function sender(array $body, array $head = []): string
+    {
+        $responseHeader = [
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json',
+          'X-API-KEY' => $this->apiKey,
+          'X-Mailer' => 'UniOne SDK-PHP agent',
+        ];
+
+        // Build body for request.
+        // @todo: Refactor this and create a special class for it.
+        $responseBody = [
+          'message' => [
+            'recipients' => [
+              [
+                'email' => $body['recipients_email'] ?? null,
+                'substitutions' => $body['recipients_substitutions'] ?? null,
+                'metadata' => $body['recipients_metadata'] ?? null,
+              ],
+            ],
+            'template_id' => $body['template_id'] ?? null,
+            'skip_unsubscribe' => $body['skip_unsubscribe'] ?? null,
+            'global_language' => $body['global_language'] ?? null,
+            'template_engine' => $body['template_engine'] ?? null,
+            'global_substitutions' => $body['global_substitutions'] ?? null,
+            'global_metadata' => $body['global_metadata'] ?? null,
+            'body' => [
+              'html' => $body['body_html'] ?? null,
+              'plaintext' => $body['body_plaintext'] ?? null,
+              'amp' => $body['body_amp'] ?? null,
+            ],
+            'subject' => $body['subject'] ?? null,
+            'from_email' => $body['from_email'] ?? null,
+            'from_name' => $body['from_name'] ?? null,
+            'reply_to' => $body['reply_to'] ?? null,
+            'track_links' => 0,
+            'track_read' => 0,
+            'headers' => $body['headers'] ?? null,
+            'attachments' => $body['attachments'] ?? null,
+            'inline_attachments' => $body['inline_attachments'] ?? null,
+            'options' => $body['options'] ?? null,
+          ],
+        ];
+
+        try {
+            // Send request.
+            $response = $this->httpClient->post('email/send.json', [
+                'headers' => $head + $responseHeader,
+                'json' => $responseBody,
+              ]
+            );
+
+            return $response->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            // handle exception or api errors.
+            return $e->getResponse()->getBody()->getContents();
+        }
+    }
 }
