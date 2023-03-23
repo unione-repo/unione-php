30ee94b097cd733ac285399ccd32c3774546f8ed
Oleksandr Tymoshchuk <o.tymoshchuk@dev-branch.com>
2023-03-23 10:56:42 +0200

    feat: Added Webmozart\Assert to validate subscribe method array keys

--- src/Api/Email.php
+++ src/Api/Email.php
@@ -7,6 +7,8 @@ namespace Unione\Api;
 use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\TransferException;
+use Webmozart\Assert\Assert;
+use Webmozart\Assert\InvalidArgumentException;
 use Unione\Model\Email as EmailData;
 use Unione\UniOneClient;
 
@@ -49,15 +51,25 @@ class Email
 
   /**
    * Send a request to the UniOne API.
-   * @param  array                $body the request body
+   * @param  array                $body the request body containing the necessary keys
+   *                                    $body = [
+   *                                    "from_email" => "user@example.com",
+   *                                    "from_name" => "string",
+   *                                    "to_email" => "user@example.com"
+   *                                    ];
    * @return array                the response with the status code
    * @throws GuzzleException
    * @throws BadResponseException
    * @throws TransferException
+   * @throws InvalidArgumentException
    */
   public function subscribe(array $body): array
   {
       $path = 'email/subscribe.json';
+
+      Assert::email($body['from_email'], 'The from_email must be an email. Got: %s');
+      Assert::string($body['from_name'], 'The from_name must be an string. Got: %s');
+      Assert::email($body['to_email'], 'The to_email must be an email. Got: %s');
 
       return $this->client->httpRequest($path, $body);
   }
