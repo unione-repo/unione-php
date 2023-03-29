fbcb89c2b85c048287e3d0ba5788d0e17f0439f3
Oleksandr Tymoshchuk <o.tymoshchuk@dev-branch.com>
2023-03-29 11:07:26 +0300

    refactor: Refactored Api\Email. Now send method gets array  and array . Added array definition comments and Assert types check

--- src/Api/Email.php
+++ src/Api/Email.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Unione\Api;
 
 use GuzzleHttp\Exception\GuzzleException;
-use Unione\Model\Email as EmailData;
 use Unione\UniOneClient;
 use Webmozart\Assert\Assert;
 
@@ -31,29 +30,55 @@ class Email
 
   /**
    * Send an email.
-   * @param  EmailData       $mail the request parameters
+   *
+   * @param array {
+   *                'recipients': array {array {'email': string}},
+   *                'body': array,
+   *                'subject': string,
+   *                'from_email': string,
+   *             } $params the request parameters
+   * @param array $headers the request headers
+   *
    * @return array           the response with the status code
    * @throws GuzzleException
+   * @see  https://docs.unione.io/en/web-api-ref#email-send Documentation of unione email-send
    */
-  public function send(EmailData $mail): array
+  public function send(array $params, array $headers = []): array
   {
-      $path = 'email/send.json';
-      $headers = $mail->getRequestHeaders();
-      $body = $mail->toArray();
+      if (!empty($params['message'])) {
+          $params = $params['message'];
+      }
 
-      return $this->client->httpRequest($path, $body, $headers);
+      Assert::isArray($params['recipients'], 'The recipients params must be an array. Got: %s');
+      Assert::isArray($params['body'], 'The body params must be an array. Got: %s');
+      Assert::string($params['subject'], 'The subject must be a string. Got: %s');
+      Assert::email($params['from_email'], 'The from_email must be an email. Got: %s');
+
+      if (!empty($params['recipients'])) {
+          foreach ($params['recipients'] as $item) {
+              Assert::email($item['email'], 'The email must be an email. Got: %s');
+          }
+      }
+
+      return $this->client->httpRequest('email/send.json', ['message' => $params], $headers);
   }
 
   /**
    * Send a subscription email.
-   * @param  array{'from_email': string, 'from_name': string, 'to_email': string} $params the request body containing the necessary keys
-   * @return array                                                                the response with the status code
+   *
+   * @param array {
+   *              'from_email': string,
+   *              'from_name': string,
+   *              'to_email': string,
+   *           } $params the request body containing the necessary keys
+   * @return array           the response with the status code
    * @throws GuzzleException
+   * @see https://docs.unione.io/en/web-api-ref#email-subscribe Documentation of unione email-subscribe.
    */
   public function subscribe(array $params): array
   {
       Assert::email($params['from_email'], 'The from_email must be an email. Got: %s');
-      Assert::string($params['from_name'], 'The from_name must be an string. Got: %s');
+      Assert::string($params['from_name'], 'The from_name must be a string. Got: %s');
       Assert::email($params['to_email'], 'The to_email must be an email. Got: %s');
 
       return $this->client->httpRequest('email/subscribe.json', $params);
