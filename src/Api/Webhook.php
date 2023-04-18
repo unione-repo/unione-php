72ba02cc5553421cbded9c963a6d70b78bfc9f4a
Oleksandr Tymoshchuk <o.tymoshchuk@dev-branch.com>
2023-04-18 10:15:52 +0300

    feat: Added webhook validate method. To the UnioneClient added getApiKey method

--- src/Api/Webhook.php
+++ src/Api/Webhook.php
@@ -31,7 +31,10 @@ class Webhook
     /**
      * Sets or edits a webhook.
      *
-     * @param array {'url': string} $params the request parameters
+     * @param array {
+     *              'url': string
+     *              'events': array {'email_status': array}
+     *            } $params the request parameters
      *
      * @return array           API response
      * @throws GuzzleException
@@ -40,7 +43,7 @@ class Webhook
     public function set(array $params): array
     {
         Assert::string($params['url'], 'The url must be a string. Got: %s');
-
+        Assert::isNonEmptyList($params['events']['email_status'], 'The email_status must be a not empty array. Got: %s');
         return $this->client->httpRequest('webhook/set.json', $params);
     }
 
@@ -86,5 +89,26 @@ class Webhook
       Assert::string($params['url'], 'The url must be a string. Got: %s');
 
       return $this->client->httpRequest('webhook/delete.json', $params);
+  }
+
+  /**
+   * Validate hook response message.
+   *
+   * @param string $message_body
+   *
+   * @return bool
+   */
+  public function validate(string $message_body): bool
+  {
+      if (empty($message_body)) {
+          return false;
+      }
+
+      $params = \json_decode($message_body, true);
+      $auth = $params['auth'];
+      $params['auth'] = $this->client->getApiKey();
+      $md5_body = \md5(\json_encode($params));
+
+      return $auth == $md5_body;
   }
 }
