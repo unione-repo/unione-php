635e02fc47d0b0bc0e2c1178517700aa2b1f93d2
Oleksandr Tymoshchuk <o.tymoshchuk@dev-branch.com>
2023-04-24 17:59:44 +0300

    docs: Changed version to 1.1.0

--- src/UnioneClient.php
+++ src/UnioneClient.php
@@ -19,7 +19,7 @@ final class UnioneClient
     /**
      * The current version of the SDK.
      */
-    private const VERSION = '1.0.0';
+    private const VERSION = '1.1.0';
 
     /**
      * The HTTP client.
